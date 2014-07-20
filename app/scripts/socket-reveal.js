61a27c8667793faa2c079ea8de9eae1ec3cda703
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-07-20 16:06:31 -0400

    added the socket for reveal

--- app/scripts/socket-reveal.js
+++ app/scripts/socket-reveal.js
@@ -1,13 +1,19 @@
 "use strict";
 
 $(function(){
-    var api=impress();
-    api.init();
+    Reveal.initialize({
+        controls: true,
+        progress: true,
+        history: true,
+        center: true,
+        theme: Reveal.getQueryHash().theme,
+        transition: Reveal.getQueryHash().transition || 'default'
+    });
 
     var host='localhost';
     var port='9001';
 
-    console.log('starting the socket impress');
+    console.log('starting the socket reveal');
     window.WebSocket=window.WebSocket||window.MozWebSocket;
     var connection=new WebSocket('ws://'+host+':'+port);
 
@@ -21,10 +27,16 @@ $(function(){
         console.log(json.action);
         switch(json.action){
             case 'next':
-                api.next();
+                Reveal.right();
                 break;  
            case 'prev':
-                api.prev();
+                Reveal.left();
+                break;
+           case 'up':
+                Reveal.up();
+                break;
+            case 'down':
+                Reveal.down();
                 break;
         }
     };
