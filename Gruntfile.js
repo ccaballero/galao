b1e256bee4e5433dbc722458f82046a580254c04
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-07-20 11:19:55 -0400

    added the grunt task for server execution

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,369 +1,138 @@
-// Generated on 2014-06-26 using generator-webapp 0.4.9
 'use strict';
 
-// # Globbing
-
-module.exports = function (grunt) {
-
-    // Load grunt tasks automatically
+module.exports=function(grunt){
     require('load-grunt-tasks')(grunt);
-
-    // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
 
-    // Configurable paths
-    var config = {
-        app: 'app',
-        dist: 'dist'
-    };
+    grunt.registerTask('serve',function(target){
+        var nodemon=grunt.util.spawn({
+            cmd:'grunt',
+            grunt:true,
+            args:'nodemon'
+        });
+        nodemon.stdout.pipe(process.stdout);
+        nodemon.stderr.pipe(process.stderr);
 
-    // Define the configuration for all the tasks
+        grunt.task.run([
+        'clean:server',
+        'concurrent:server',
+        'autoprefixer',
+        'connect:livereload',
+        'watch'
+        ]);
+    });
+
     grunt.initConfig({
+        clean:{
+            server:'.tmp'
+        },
 
-        // Project settings
-        config: config,
+        concurrent:{
+            server:[
+                'sass:server',
+                'copy:styles'
+            ]
+        },
 
-        // Watches files for changes and runs tasks based on the changed files
-        watch: {
-            bower: {
-                files: ['bower.json'],
-                tasks: ['bowerInstall']
-            },
-            js: {
-                files: ['<%= config.app %>/scripts/{,*/}*.js'],
-                tasks: ['jshint'],
-                options: {
-                    livereload: true
-                }
-            },
-            gruntfile: {
-                files: ['Gruntfile.js']
-            },
-            sass: {
-                files: ['<%= config.app %>/styles/{,*/}*.{scss,sass}'],
-                tasks: ['sass:server', 'autoprefixer']
-            },
-            styles: {
-                files: ['<%= config.app %>/styles/{,*/}*.css'],
-                tasks: ['newer:copy:styles', 'autoprefixer']
-            },
-            livereload: {
-                options: {
-                    livereload: '<%= connect.options.livereload %>'
-                },
-                files: [
-                    '<%= config.app %>/{,*/}*.html',
-                    'examples/{,*/}*.html',
-                    '.tmp/styles/{,*/}*.css',
-                    '<%= config.app %>/images/{,*/}*'
+        sass:{
+            options:{
+                includePaths:[
+                    'bower_components'
                 ]
+            },
+            server:{
+                files:[{
+                    expand:true,
+                    cwd:'app/styles',
+                    src:['*.scss'],
+                    dest:'.tmp/styles',
+                    ext:'.css'
+                }]
             }
         },
 
-        // The actual grunt server settings
-        connect: {
-            options: {
-                port: 9000,
-                open: true,
-                livereload: 35729,
-                // Change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
+        copy:{
+            styles:{
+                expand:true,
+                dot:true,
+                cwd:'app/styles',
+                dest:'.tmp/styles/',
+                src:'{,*/}*.css'
+            }
+        },
+
+        autoprefixer:{
+            options:{
+                browsers:['last 1 version']
             },
-            livereload: {
-                options: {
-                    middleware: function(connect) {
+        },
+
+        connect:{
+            options:{
+                port:9000,
+                open:true,
+                livereload:35729,
+                hostname:'localhost'
+            },
+            livereload:{
+                options:{
+                    middleware:function(connect){
                         return [
                             connect.static('.tmp'),
-                            connect().use('/bower_components', connect.static('./bower_components')),
-                            connect().use('/examples', connect.static('./examples')),
-                            connect.static(config.app)
+                            connect().use('/bower_components',
+                                connect.static('./bower_components')),
+                            connect().use('/examples',
+                                connect.static('./examples')),
+                            connect.static('app')
                         ];
                     }
                 }
             },
-            dist: {
-                options: {
-                    base: '<%= config.dist %>',
-                    livereload: false
+        },
+
+        watch:{
+            js:{
+                files:['app/scripts/{,*/}*.js'],
+                tasks:['jshint'],
+                options:{
+                    livereload:true
                 }
-            }
-        },
-
-        // Empties folders to start fresh
-        clean: {
-            dist: {
-                files: [{
-                    dot: true,
-                    src: [
-                        '.tmp',
-                        '<%= config.dist %>/*',
-                        '!<%= config.dist %>/.git*'
-                    ]
-                }]
             },
-            server: '.tmp'
-        },
-
-        // Make sure code styles are up to par and there are no obvious mistakes
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
+            gruntfile:{
+                files:['Gruntfile.js']
             },
-            all: [
-                'Gruntfile.js',
-                '<%= config.app %>/scripts/{,*/}*.js',
-                '!<%= config.app %>/scripts/vendor/*',
-            ]
-        },
-
-        // Compiles Sass to CSS and generates necessary files if requested
-        sass: {
-            options: {
-                includePaths: [
-                    'bower_components'
+            sass:{
+                files:['app/styles/{,*/}*.{scss,sass}'],
+                tasks:['sass:server','autoprefixer']
+            },
+            styles:{
+                files:['app/styles/{,*/}*.css'],
+                tasks:['newer:copy:styles','autoprefixer']
+            },
+            livereload:{
+                options:{
+                    livereload:'<%= connect.options.livereload %>'
+                },
+                files:[
+                    'app/{,*/}*.html',
+                    'examples/{,*/}*.html',
+                    '.tmp/styles/{,*/}*.css',
+                    'app/images/{,*/}*'
                 ]
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= config.app %>/styles',
-                    src: ['*.scss'],
-                    dest: '.tmp/styles',
-                    ext: '.css'
-                }]
-            },
-            server: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= config.app %>/styles',
-                    src: ['*.scss'],
-                    dest: '.tmp/styles',
-                    ext: '.css'
-                }]
             }
         },
 
-        // Add vendor prefixed styles
-        autoprefixer: {
-            options: {
-                browsers: ['last 1 version']
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '.tmp/styles/',
-                    src: '{,*/}*.css',
-                    dest: '.tmp/styles/'
-                }]
-            }
-        },
-
-        // Automatically inject Bower components into the HTML file
-        bowerInstall: {
-            app: {
-                src: ['<%= config.app %>/index.html'],
-                exclude: ['bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap.js']
-            },
-            sass: {
-                src: ['<%= config.app %>/styles/{,*/}*.{scss,sass}']
-            }
-        },
-
-        // Renames files for browser caching purposes
-        rev: {
-            dist: {
-                files: {
-                    src: [
-                        '<%= config.dist %>/scripts/{,*/}*.js',
-                        '<%= config.dist %>/styles/{,*/}*.css',
-                        '<%= config.dist %>/images/{,*/}*.*',
-                        '<%= config.dist %>/styles/fonts/{,*/}*.*',
-                        '<%= config.dist %>/*.{ico,png}'
-                    ]
+        nodemon:{
+            dev:{
+                script:'server.js',
+                options:{
+                    nodeArgs:['--debug'],
+                    env:{
+                        PORT:'9001'
+                    }
                 }
             }
-        },
-
-        // Reads HTML for usemin blocks to enable smart builds that automatically
-        // concat, minify and revision files. Creates configurations in memory so
-        // additional tasks can operate on them
-        useminPrepare: {
-            options: {
-                dest: '<%= config.dist %>'
-            },
-            html: '<%= config.app %>/index.html'
-        },
-
-        // Performs rewrites based on rev and the useminPrepare configuration
-        usemin: {
-            options: {
-                assetsDirs: ['<%= config.dist %>', '<%= config.dist %>/images']
-            },
-            html: ['<%= config.dist %>/{,*/}*.html'],
-            css: ['<%= config.dist %>/styles/{,*/}*.css']
-        },
-
-        // The following *-min tasks produce minified files in the dist folder
-        imagemin: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= config.app %>/images',
-                    src: '{,*/}*.{gif,jpeg,jpg,png}',
-                    dest: '<%= config.dist %>/images'
-                }]
-            }
-        },
-
-        svgmin: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= config.app %>/images',
-                    src: '{,*/}*.svg',
-                    dest: '<%= config.dist %>/images'
-                }]
-            }
-        },
-
-        htmlmin: {
-            dist: {
-                options: {
-                    collapseBooleanAttributes: true,
-                    collapseWhitespace: true,
-                    removeAttributeQuotes: true,
-                    removeCommentsFromCDATA: true,
-                    removeEmptyAttributes: true,
-                    removeOptionalTags: true,
-                    removeRedundantAttributes: true,
-                    useShortDoctype: true
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= config.dist %>',
-                    src: '{,*/}*.html',
-                    dest: '<%= config.dist %>'
-                }]
-            }
-        },
-
-        // By default, your `index.html`'s <!-- Usemin block --> will take care of
-        // minification. These next options are pre-configured if you do not wish
-        // to use the Usemin blocks.
-        // cssmin: {
-        //     dist: {
-        //         files: {
-        //             '<%= config.dist %>/styles/main.css': [
-        //                 '.tmp/styles/{,*/}*.css',
-        //                 '<%= config.app %>/styles/{,*/}*.css'
-        //             ]
-        //         }
-        //     }
-        // },
-        // uglify: {
-        //     dist: {
-        //         files: {
-        //             '<%= config.dist %>/scripts/scripts.js': [
-        //                 '<%= config.dist %>/scripts/scripts.js'
-        //             ]
-        //         }
-        //     }
-        // },
-        // concat: {
-        //     dist: {}
-        // },
-
-        // Copies remaining files to places other tasks can use
-        copy: {
-            dist: {
-                files: [{
-                    expand: true,
-                    dot: true,
-                    cwd: '<%= config.app %>',
-                    dest: '<%= config.dist %>',
-                    src: [
-                        '*.{ico,png,txt}',
-                        '.htaccess',
-                        'images/{,*/}*.webp',
-                        '{,*/}*.html',
-                        'styles/fonts/{,*/}*.*'
-                    ]
-                }]
-            },
-            styles: {
-                expand: true,
-                dot: true,
-                cwd: '<%= config.app %>/styles',
-                dest: '.tmp/styles/',
-                src: '{,*/}*.css'
-            }
-        },
-
-        // Generates a custom Modernizr build that includes only the tests you
-        // reference in your app
-        modernizr: {
-            dist: {
-                devFile: 'bower_components/modernizr/modernizr.js',
-                outputFile: '<%= config.dist %>/scripts/vendor/modernizr.js',
-                files: {
-                    src: [
-                        '<%= config.dist %>/scripts/{,*/}*.js',
-                        '<%= config.dist %>/styles/{,*/}*.css',
-                        '!<%= config.dist %>/scripts/vendor/*'
-                    ]
-                },
-                uglify: true
-            }
-        },
-
-        // Run some tasks in parallel to speed up build process
-        concurrent: {
-            server: [
-                'sass:server',
-                'copy:styles'
-            ],
-            dist: [
-                'sass',
-                'copy:styles',
-                'imagemin',
-                'svgmin'
-            ]
         }
     });
-
-
-    grunt.registerTask('serve', function (target) {
-        if (target === 'dist') {
-            return grunt.task.run(['build', 'connect:dist:keepalive']);
-        }
-
-        grunt.task.run([
-            'clean:server',
-            'concurrent:server',
-            'autoprefixer',
-            'connect:livereload',
-            'watch'
-        ]);
-    });
-
-    grunt.registerTask('build', [
-        'clean:dist',
-        'useminPrepare',
-        'concurrent:dist',
-        'autoprefixer',
-        'concat',
-        'cssmin',
-        'uglify',
-        'copy:dist',
-        'modernizr',
-        'rev',
-        'usemin',
-        'htmlmin'
-    ]);
-
-    grunt.registerTask('default', [
-        'newer:jshint',
-        'build'
-    ]);
 };
+
--- server.js
+++ server.js
@@ -1,33 +1,41 @@
 "use strict";
 
-var WebSocketServer = require('websocket').server;
-var http = require('http');
-var port = 9091;
+var WebSocketServer=require('websocket').server;
+var http=require('http');
 
-var server = http.createServer(function(request,response){
+var port;
+if(process.env.PORT){
+    port=process.env.PORT
+} else {
+    port=9001;
+}
+
+var server=http.createServer(function(request,response){
     console.log((new Date())+' HTTP server. URL'+request.url+' requested.');
 });
 
-server.listen(port, function() {
+server.listen(port,function(){
     console.log((new Date())+' Server is listening on port '+port);
 });
 
 var clients=new Array();
 
-var wsServer = new WebSocketServer({httpServer:server});
+var wsServer=new WebSocketServer({httpServer:server});
 wsServer.on('request',function(request){
     var connection=request.accept(null,request.origin);
 
-    console.log('connection established');
+    console.log((new Date())+' Connection Established');
     clients.push(connection);
 
-    connection.on('message', function(message) {
-        console.log(message);
-        for (var i = 0; i < clients.length; i++) {
+    connection.on('message',function(message){
+        console.log((new Date())+' Action triggered '+message);
+        for(var i=0;i<clients.length;i++){
             clients[i].sendUTF(message.utf8Data);
         }
     });
 
-    connection.on('close', function(connection) {});
+    connection.on('close',function(connection){
+        console.log((new Date())+' Client Disconnected');
+    });
 });
 
