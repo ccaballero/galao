5b6a0486ae728c9742732571d3f62085d8eb739e
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-07-24 10:51:06 -0400

    Added the connection message and status of connection to the nav.
        modified:   app/control.html
        modified:   app/scripts/control.js
        modified:   app/styles/control.scss
        modified:   app/styles/overlay.scss

--- app/scripts/control.js
+++ app/scripts/control.js
@@ -6,28 +6,6 @@ var port='9001';
 $(function(){
     window.WebSocket=window.WebSocket||window.MozWebSocket;
 
-    var request=function(connection,task){
-        console.log(task);
-        connection.send(JSON.stringify({action:task}));
-    };
-
-    var connection;
-    var connect=function(){
-        connection=new WebSocket('ws://'+host+':'+port);
-        connection.onopen=function(){
-            console.log('starting the remote control over '+host+':'+port);
-        };
-        connection.onerror=function(error){
-            console.log('fail the connection in '+host+':'+port);
-        };
-        connection.onmessage=function(message){};
-    };
-
-    $('.up').click(function(){request(connection,'up')});
-    $('.down').click(function(){request(connection,'down')});
-    $('.prev').click(function(){request(connection,'prev')});
-    $('.next').click(function(){request(connection,'next')});
-
     var overlay = new Overlay(document.getElementsByClassName('overlay')[0], {
             class_name: 'overlay',
             background_class_name: 'overlay-background',
@@ -44,6 +22,37 @@ $(function(){
             }
         }
     );
+
+    var request=function(connection,task){
+        console.log(task);
+        connection.send(JSON.stringify({action:task}));
+    };
+
+    var connection;
+    var connect=function(){
+        connection=new WebSocket('ws://'+host+':'+port);
+        connection.onopen=function(){
+            console.log('starting the remote control over '+host+':'+port);
+            overlay.hide();
+            $('.offline').hide();
+            $('.online').show();
+        };
+        connection.onerror=function(error){
+            console.log('fail the connection to '+host+':'+port);
+            $('.overlay p.message').html(
+                'Fail the connection to: '+host+':'+port);
+            $('.online').hide();
+            $('.offline').show();
+        };
+        connection.onmessage=function(message){};
+    };
+
+    $('.online').hide();
+    $('.up').click(function(){request(connection,'up')});
+    $('.down').click(function(){request(connection,'down')});
+    $('.prev').click(function(){request(connection,'prev')});
+    $('.next').click(function(){request(connection,'next')});
+
     $('.settings').click(function(){
         overlay.append_content($('#connect').html());
         $('input[name="host"]').val(host);
@@ -54,7 +63,6 @@ $(function(){
             host=$('input[name="host"]').val();
             port=$('input[name="port"]').val();
             connect();
-            overlay.hide();
         });
     });
 });
